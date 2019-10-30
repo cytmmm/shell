004c37cc6ef48d8647fab9b015b9e628149e06d5
陈逸韬 <chenyt@orderhandler.com>
2019-10-30 18:08:06 +0800

    优化语法

--- getLastModifyTime.sh
+++ getLastModifyTime.sh
@@ -5,7 +5,7 @@
 
 FILE_NAME=$1
 
-LAST_MODIFY_TIMESTAMP=`stat -c %Y  $FILE_NAME`
-formart_date=`date '+%Y-%m-%d %H:%M:%S' -d @$LAST_MODIFY_TIMESTAMP`
-echo $formart_date
+LAST_MODIFY_TIMESTAMP=$(stat -c %Y  "$FILE_NAME")
+formart_date=$(date '+%Y-%m-%d %H:%M:%S' -d @"$LAST_MODIFY_TIMESTAMP")
+echo "$formart_date"
 exit 0
